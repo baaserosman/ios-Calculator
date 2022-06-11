8b70efaafce918f6c34fa687058ccf25790f412c
baaserosman <baaser.osman@gmail.com>
2022-06-11 11:29:07 +0200

    appjs modified

--- app.js
+++ app.js
@@ -1,174 +1,137 @@
-//* ======================================================
-//*                     IOS CALCULATOR
-//* ======================================================
+const keys = document.querySelector(".keys");
+const displayUp = document.querySelector(".display-up");
+const displayDown = document.querySelector(".display-down");
 
-//? Selectors
-//! We can use getElementsByClassName selector to select multiple html elements. But, if we want to iterate these elements, we can use Array.from() method or spread/rest (...) operator.
-// const numberButtons = document.getElementsByClassName("num");
-// Array.from(numberButtons).forEach((element) => {
-//   console.log(element);
-// });
-// [...numberButtons].forEach((element) => {
-//   console.log(element);
-// });
+let displayUpValue = 0;
+let displayDownValue = 0;
+let isControl = true;
+let operator = [];
+let pointControl = true;
 
-const numberButtons = document.querySelectorAll(".num");
-const operationButtons = document.querySelectorAll(".operator");
-const equalsButton = document.querySelector(".equal");
-const acButton = document.querySelector(".ac");
-const pmButton = document.querySelector(".pm");
-const percentButton = document.querySelector(".percent");
-const prevDisp = document.querySelector(".previous-display");
-const currDisp = document.querySelector(".current-display");
+// let operatorPrev;
+displayDown.innerHTML = displayDownValue;
 
-//? Operator variables
-let previousOperand = "";
-let currentOperand = "";
-let operation = "";
+//click any button
+keys.addEventListener("click", (e) => {
+  //so that it doesn't take action when the space is clicked
+  if (e.target.parentElement.classList.contains("box")) {
+    //write to the number
+    if (e.target.parentElement.classList.contains("number")) {
+      if (!isControl) {
+        // "=" işlemi yaptıktan sonra resetlemek için
+        displayDownValue = 0;
+        displayDown.innerHTML = "";
+      }
+      displayDownValue = +(displayDown.innerHTML + e.target.innerHTML);
+      displayDown.innerHTML = displayDownValue;
+      isControl = true;
+      pointControl = true;
+    }
 
-//* After equal or percent buttons are pressed and then new number entered, we should clear the current display. This boolean variable is used to check these buttons are pressed or not
-let equalOrPercentBtnPressed = false;
+    //to make the current number positive or negative
+    if (e.target.innerHTML == "±") {
+      displayDownValue *= -1;
+      displayDown.innerHTML = displayDownValue;
+    }
 
-//?numbers and decimal buttons event
-numberButtons.forEach((number) => {
-  number.addEventListener("click", () => {
-    appendNumber(number.textContent);
-    updateDisplay();
-  });
-});
+    // displayDown.innerHTML = displayDownValue;
+    // point button
+    if (e.target.innerHTML == ".") {
+      if (!isControl) {
+        // "=" işlemi yaptıktan sonra resetlemek için
+        displayDownValue = 0;
+        displayDown.innerHTML = "";
+      }
+      if (displayDownValue % 1 == 0 && pointControl) {
+        displayDown.innerHTML += ".";
+        pointControl = false;
+      }
+      isControl = true;
+    }
 
-//?Operator button event
-operationButtons.forEach((op) => {
-  op.addEventListener("click", () => {
-    chooseOperator(op.textContent);
-    updateDisplay();
-  });
-});
+    //operators buttons
+    if (e.target.parentElement.classList.contains("operator")) {
+      if (e.target.innerHTML == "x") {
+        operator.push("*");
+      } else if (e.target.innerHTML == "÷") {
+        operator.push("/");
+      } else {
+        operator.push(e.target.innerHTML);
+      }
 
-//? Equal button event
-equalsButton.addEventListener("click", () => {
-  compute();
-  updateDisplay();
-  equalOrPercentBtnPressed = true;
-});
+      if (displayUp.innerHTML) {
+        if (displayDownValue) {
+          displayUpValue = process(
+            displayUpValue,
+            displayDownValue,
+            operator[operator.length - 2]
+          );
+          displayUp.innerHTML = displayUpValue + operator[operator.length - 1];
+        } else {
+          displayUp.innerHTML = displayUpValue + operator[operator.length - 1];
+        }
 
-//? All Clear(AC) button event
-acButton.addEventListener("click", () => {
-  clear();
-  updateDisplay();
-});
+        displayDown.innerHTML = "";
+        displayDownValue = 0;
+      } else if (pointControl) {
+        displayUpValue = displayDownValue;
+        displayDownValue = 0;
+        displayUp.innerHTML =
+          displayDown.innerHTML + operator[operator.length - 1];
+        displayDown.innerHTML = "";
+      }
+    }
 
-//? plus-minus(+-) button event
-pmButton.addEventListener("click", () => {
-  plusMinus();
-  updateDisplay();
-});
+    // result(=) button
+    if (e.target.innerHTML == "=") {
+      if (displayUp.innerHTML && displayDown.innerHTML) {
+        displayDownValue = process(
+          displayUpValue,
+          displayDownValue,
+          operator[operator.length - 1]
+        );
+        displayDown.innerHTML = displayDownValue;
+        displayUp.innerHTML = "";
+        isControl = false;
+      }
+    }
 
-//? percent (%) button event
-percentButton.addEventListener("click", () => {
-  percent();
-  updateDisplay();
-  equalOrPercentBtnPressed = true;
-});
-
-//? When number and decimal buttons are clicked, this function appends the number's textContent to the operands
-const appendNumber = (num) => {
-  //? if our number includes . and user reenters ., it returns
-  if (num === "." && currentOperand.includes(".")) return;
-
-  //? if user enters 0 and then reenter 0 , it returns
-  if (currentOperand === "0" && num === "0") return;
-
-  //? if user enters 0  then dont enter 0 and . , it display this entered value.
-  if (currentOperand === "0" && num !== "0" && num !== ".") {
-    currentOperand = num;
-    return;
+    // reset button
+    if (e.target.innerHTML == "AC") {
+      console.log("AC");
+      displayUp.innerHTML = "";
+      displayDown.innerHTML = 0;
+      displayDownValue = 0;
+      displayUpValue = 0;
+    }
   }
-  //? if user enters more than 10 digit,it returns
-  if (currentOperand.length > 10) return;
+});
 
-  //? if equal or percent btn is pressed and then user enter new number, it display just new entered number
-  if (equalOrPercentBtnPressed) {
-    equalOrPercentBtnPressed = false; //* clear for next usage
-    currentOperand = num;
-    return;
-  }
-  //? otherwise,it concatinates all numbers to display them
-  currentOperand += num;
-};
+// arithmetic process function
+const process = function (val1, val2, ops) {
+  let result;
 
-//? Display the numbers and computation
-const updateDisplay = () => {
-  //? if computation or number is too long, it trims
-  if (currentOperand.toString().length > 12) {
-    currentOperand = currentOperand.toString().slice(0, 12);
-  }
-  currDisp.textContent = currentOperand;
-
-  if (operation != null) {
-    prevDisp.textContent = `${previousOperand} ${operation}`;
-  } else {
-    prevDisp.textContent = "";
-  }
-};
-const chooseOperator = (op) => {
-  //? if user clicks any operator button without entering any number,it returns
-  if (currentOperand === "") return;
-
-  //? if user enter any number then press any operator button, it computes
-  if (previousOperand) {
-    compute();
-  }
-
-  //? variable swapping
-  operation = op;
-  previousOperand = currentOperand;
-  currentOperand = "";
-};
-
-//? compute the result
-const compute = () => {
-  let computation;
-  const prev = parseFloat(previousOperand);
-  const current = parseFloat(currentOperand);
-  if (isNaN(prev) || isNaN(current)) return;
-  switch (operation) {
+  switch (ops) {
+    case "*":
+      result = val1 * val2;
+      break;
+    case "/":
+      result = val1 / val2;
+      break;
     case "+":
-      computation = prev + current;
+      result = val1 + val2;
       break;
     case "-":
-      computation = prev - current;
+      result = val1 - val2;
       break;
-    case "x":
-      computation = prev * current;
+    case "%":
+      result = (val1 / 100) * val2;
       break;
-    case "÷":
-      computation = prev / current;
-      break;
-    default:
-      return;
   }
-  currentOperand = computation;
-  operation = "";
-  previousOperand = "";
-  //? if equal button is clicked, we should reset operation and previousOperand for next usage
-};
 
-//? when ac button is clicked, clear all displays
-const clear = () => {
-  operation = "";
-  previousOperand = "";
-  currentOperand = "";
-};
+  if (result % 1 != 0) {
+    result = +result.toFixed(2);
+  }
 
-//? when pm button is clicked, toggle the polarity of entered value
-const plusMinus = () => {
-  if (!currentOperand) return;
-  currentOperand = currentOperand * -1;
-};
-
-//? when % button is clicked
-const percent = () => {
-  if (!currentOperand) return;
-  currentOperand = currentOperand / 100;
+  return result;
 };
